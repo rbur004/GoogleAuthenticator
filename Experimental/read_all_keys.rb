233e9071c4c86d56536aa47a555e6365e3d4b49a
robertburrowes <r.burrowes@auckland.ac.nz>
2021-09-24 14:42:28 +1200

    Change URI.unencode to CGI.unencode.
    Rubocop'd all source

--- Experimental/read_all_keys.rb
+++ Experimental/read_all_keys.rb
@@ -1,15 +1,14 @@
 #!/usr/local/bin/ruby
 require 'keychain'
 
-#Test, by reading it back keys. Will get prompted for local account password by Mac OS X.
-#Keychain.generic_passwords.where(:service => 'Google_Authenticator', :account => "#{account}").all.each do |p|
-#rbur004=Keychain.open('googleauthenticator.keychain')
+# Test, by reading it back keys. Will get prompted for local account password by Mac OS X.
+# Keychain.generic_passwords.where(:service => 'Google_Authenticator', :account => "#{account}").all.each do |p|
+# rbur004=Keychain.open('googleauthenticator.keychain')
 
-Keychain.generic_passwords.where(:service => 'Google_Authenticator').all.each do |p|
+Keychain.generic_passwords.where(service: 'Google Authenticator').all.each do |p|
   begin
     puts "#{p.label}, #{p.account}, #{p.password}"
-  rescue Exception => e
-    puts "error: e"
+  rescue StandardError => e
+    puts "error: #{e}"
   end
 end
-
