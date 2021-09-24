d49c7374a5be289c460142982d79cee7dc50e99d
robertburrowes <r.burrowes@auckland.ac.nz>
2021-09-24 16:47:59 +1200

    Reformat comment spacing

--- older_cli_code/gauth_scan_qr_code.rb
+++ older_cli_code/gauth_scan_qr_code.rb
@@ -17,9 +17,7 @@ def png_to_y800(image:)
   (0...image.height).each do |h|
     (0...image.width).each do |w|
       v = image[w, h]
-      y800 << ((((ChunkyPNG::Color.r(v) * 0.21) +
-                (ChunkyPNG::Color.g(v) * 0.72) +
-                (ChunkyPNG::Color.b(v) * 0.07)).round ) & 0xFF ).chr
+      y800 << ((((ChunkyPNG::Color.r(v) * 0.21) + (ChunkyPNG::Color.g(v) * 0.72) + (ChunkyPNG::Color.b(v) * 0.07)).round ) & 0xFF ).chr
     end
   end
   return y800
@@ -33,7 +31,7 @@ def save_key(issuer:, account:, key:)
   begin
     # Keychain looks to be using (account,service) as it's unique key. Adding fails silently if there is a collision.
     # Therefore, we will always ensure account is of the form account@issuer (unless account is already qualified with a domain name)
-    issuer = issuer.split('@')[1] if issuer =~ /@/      # Set issuer to the domain part, if it is in the form user@domain style
+    issuer = issuer.split('@')[1] if issuer =~ /@/        # Set issuer to the domain part, if it is in the form user@domain style
     account = "#{account}@#{issuer}" # if account !~ /@/  # Set Account to account@issuer, even if the is no @ in the account string
 
     Keychain.generic_passwords.create(service: 'Google Authenticator', # fills in where field in keychain (and name, if no label)
